683aa567e8492b0eac4aa1fe8e1739f03d5afa02
Akramul Hoque <akramulhoque.engineer@gmail.com>
2023-08-28 18:48:55 +0600

    Product Model, Migration & Controller Created

--- Backend/app/Models/Category.php
+++ Backend/app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Product;
 
 class Category extends Model
 {
@@ -13,4 +14,8 @@ class Category extends Model
         'slug',
         'description'
     ];
+
+    public function products(){
+        return $this->hasMany(Product::class);
+    }
 }
