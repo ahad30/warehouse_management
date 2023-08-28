30888de08b4d220f2c97633087bb9455633da476
Akramul Hoque <akramulhoque.engineer@gmail.com>
2023-08-28 17:39:12 +0600

    Categories CRUD completed

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/categories', [CategoryController::class, 'index']);
+    Route::post('/categories/store', [CategoryController::class, 'store']);
+    Route::get('/categories/edit/{id}', [CategoryController::class, 'edit']);
+    Route::put('/categories/update/{id}', [CategoryController::class, 'update']);
+    Route::delete('/categories/delete/{id}', [CategoryController::class, 'distroy']);
+});
+
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
