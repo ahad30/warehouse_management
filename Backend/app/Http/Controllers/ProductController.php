69421670a662dbd6f58c5e794017569aaf9e0538
Akramul Hoque <akramulhoque.engineer@gmail.com>
2023-08-29 19:20:04 +0600

    Product CRUD compleated successully

--- Backend/app/Http/Controllers/ProductController.php
+++ Backend/app/Http/Controllers/ProductController.php
@@ -15,7 +15,16 @@ class ProductController extends Controller
         $products = Product::orderBy('id', 'DESC')->get();
 
         return response()->json([
-            'success' => $products
+            'products' => $products
+        ]);
+    }
+
+    // create
+    public function create(){
+        $categories = Category::all();
+        
+        return response()->json([
+            'categories' => $categories
         ]);
     }
 
@@ -62,5 +71,57 @@ class ProductController extends Controller
             'product' => $product,
             'categories' => $categories,
         ]);
+    }
+
+    // update
+    public function update(Request $request, $id){
+        $product = Product::findOrFail($id);
+
+        $codeValidation = Validator::make($request->all(),[
+            'name' => ['required', 'string', 'max:255'],
+            'code' => ['nullable'],
+            'category_id' => ['required'],
+            'price' => ['required'],
+            'unit' => ['required'],
+            'desc' => ['nullable'],
+        ]);
+
+        if($codeValidation->fails())
+        {
+            return response()->json([
+                'errors'=> $codeValidation->errors()
+            ],500);
+        }
+
+        $product->update([
+            'name' => $request->name,
+            'slug' => Str::slug($request->name),
+            'code' => $request->code,
+            'category_id' => $request->category_id,
+            'price' => $request->price,
+            'unit' => $request->unit,
+            'desc' => $request->desc,
+        ]);
+
+        return response()->json([
+            'success' => 'Product detail successfully updated'
+        ]);
+    }
+
+    // distroy 
+    public function distroy($id){
+        $product = Product::findOrFail($id);
+
+        if($product){
+            $product->delete();
+
+            return response()->json([
+                'success'=> 'Product successfully deleted'
+            ],201);
+        }else{
+            return response()->json([
+                'errors'=> 'Product not found'
+            ],500);
+        }
     }
 }
