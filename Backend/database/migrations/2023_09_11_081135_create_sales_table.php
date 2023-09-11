9776aa37958e51d951370e9b074b0de96b6fcd70
Akramul Hoque <akramulhoque.engineer@gmail.com>
2023-09-11 18:48:50 +0600

    Invoice Initial stage

---
+++ Backend/database/migrations/2023_09_11_081135_create_sales_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->string('invoice_no')->unique();
+            $table->unsignedBigInteger('customer_id')->nullable();
+            $table->string('invoice_date')->nullable();
+            $table->string('company_name')->nullable();
+            $table->string('company_email')->nullable();
+            $table->string('company_phone')->nullable();
+            $table->string('company_address')->nullable();
+            $table->string('customer_name')->nullable();
+            $table->string('customer_email')->nullable();
+            $table->string('customer_phone')->nullable();
+            $table->string('customer_address')->nullable();
+            $table->string('discount')->nullable();
+            $table->string('shipping')->nullable();
+            $table->string('total');
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+};
