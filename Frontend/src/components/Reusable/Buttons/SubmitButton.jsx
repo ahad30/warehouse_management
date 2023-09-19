a9d22fb0dcff021896e9d7a25ea40213b817a2cc
MORSHED-HOME <morshed952640@gmail.com>
2023-09-19 23:15:21 +0600

    Add Customer done with Error handling

--- Frontend/src/components/Reusable/Buttons/SubmitButton.jsx
+++ Frontend/src/components/Reusable/Buttons/SubmitButton.jsx
@@ -2,7 +2,7 @@ import { node, string } from "prop-types";
 
 const SubmitButton = ({ title, icon }) => {
   return (
-    <div className="flex justify-center items-center my-5 cursor-pointer">
+    <div className="flex justify-center items-center my-5">
       <div className="flex items-center gap-x-2 btn btn-primary w-full sm:w-fit ">
         <span>{icon}</span>
         <input className="cursor-pointer text-lg" type="submit" value={title} />
