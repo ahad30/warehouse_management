4ab78bf0e184039e00d5478de4da5f1abdd66dab
Nurul Komor <58949982+nurul-komor@users.noreply.github.com>
2023-09-26 16:13:13 +0600

    verify token middleware added

--- Backend/app/Http/Middleware/VerifyJwtTokenMiddleware.php
+++ Backend/app/Http/Middleware/VerifyJwtTokenMiddleware.php
@@ -4,11 +4,10 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Carbon\Carbon;
-use App\Models\User;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class VerifyAdminMiddleware
+class VerifyJwtTokenMiddleware
 {
     /**
      * Handle an incoming request.
@@ -18,9 +17,9 @@ class VerifyAdminMiddleware
     public function handle(Request $request, Closure $next): Response
     {
         $user = auth()->user();
-        try {
-            $userWithRole = User::where('id', $user->id)->with('getRole')->first();
-            $tokenExpireTime = strtotime($userWithRole->token_expire_time);
+
+        if (!is_null($user)) {
+            $tokenExpireTime = strtotime($user->token_expire_time);
             $timeNow = strtotime(Carbon::now());
             $diff = $tokenExpireTime - $timeNow;
             if ($diff <= 0) {
@@ -29,19 +28,7 @@ class VerifyAdminMiddleware
                     'message' => 'Unauthorized or Access token has expired'
                 ], 401);
             }
-            if ($user != null && $userWithRole->getRole->role == 'admin') {
-                return $next($request);
-            }
-        } catch (\Exception $e) {
-            return response()->json([
-                'status' => false,
-                'message' => $e->getMessage(),
-            ], 401);
+            return $next($request);
         }
-
-        return response()->json([
-            'status' => false,
-            'message' => "Unauthorized You don't have permission"
-        ], 401);
     }
 }
