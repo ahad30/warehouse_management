bc007ab618e02c29510100f19a5059b415ba6d5d
MORSHED-HOME <morshed952640@gmail.com>
2023-09-27 15:51:29 +0600

    loading update

--- Frontend/src/components/Reusable/useLoading/UseLoading.jsx
+++ Frontend/src/components/Reusable/useLoading/UseLoading.jsx
@@ -1,7 +1,7 @@
 const UseLoading = () => {
   return (
     <div className="flex items-center justify-center mt-10">
-      <span className="loading loading-spinner loading-lg"></span>
+      <span className="loading loading-spinner loading-lg "></span>
     </div>
   );
 };
