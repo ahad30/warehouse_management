a2268fe1e583dfbc408969d4be902ab76fe1f553
MORSHED-HOME <morshed952640@gmail.com>
2023-10-03 23:18:02 +0600

    Add brand with image upload bug fixed private route condition changed

--- Frontend/src/routes/PrivateRoute.jsx
+++ Frontend/src/routes/PrivateRoute.jsx
@@ -1,10 +1,11 @@
 import { node } from "prop-types";
-import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
-  const { user } = useSelector((state) => state?.auth);
-  if (!user?.jwt_token) {
+  
+  let token = localStorage.getItem('access_token')
+  // if (!user?.jwt_token) {
+  if (!token) {
     return <Navigate to={"/login"}></Navigate>;
   }
   return children;
