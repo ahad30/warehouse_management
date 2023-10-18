ce1a4d7d8bd1748723c7f6d8ca566307d3ff04a5
unknown <naimurk781@gmail.com>
2023-10-18 12:11:09 +0600

    total calcutaion color fixed

--- Frontend/src/Dashboard/Report/Analytics/TotalCalculations/TotalCalculations.jsx
+++ Frontend/src/Dashboard/Report/Analytics/TotalCalculations/TotalCalculations.jsx
@@ -12,6 +12,7 @@ import { Link } from "react-router-dom";
 const TotalCalculations = () => {
   // Fetch the dashboard summary data using a query
   const { data } = useGetDashboardSummaryQuery();
+  // const [items, setItems]= useState([])
 
   // Fetch default settings using a query
   const { data: defaultSettings } = useGetDefaultSettingsQuery();
@@ -30,73 +31,85 @@ const TotalCalculations = () => {
   }, [data?.status, data?.data]);
 
   // Define an array of items to display in the summary
-  const items = [
-    {
-      img: (
-        <AiOutlineDollarCircle
-          className="text-white"
-          size={30}
-        ></AiOutlineDollarCircle>
-      ),
-      count: `${defaultSettings?.settings?.currency || "$"}${
-        summary?.totalRevenue
-      }`,
-      text: "Total Revenue",
-      iconBackgroundColor: "#EA580C",
-    },
-    {
-      img: <VscGraph className="text-white" size={30}></VscGraph>,
-      count: summary?.totalSales,
-      text: "Total Sales",
-      iconBackgroundColor: "#EFBF02",
-      path: "/dashboard/invoice",
-    },
-    {
-      img: <BiUserCircle className="text-white" size={30}></BiUserCircle>,
-      count: summary?.totalUsers,
-      text: "Total Users",
-      iconBackgroundColor: "#8B5CF6",
-      path: "/dashboard/user",
-    },
-    {
-      img: <FiUsers className="text-white" size={30}></FiUsers>,
-      count: summary?.totalCustomers,
-      text: "Total Customers",
-      iconBackgroundColor: "#16A34A",
-      path: "/dashboard/customer",
-    },
-    {
-      img: <BsFillCartFill className="text-white" size={30}></BsFillCartFill>,
-      count: summary?.totalProducts,
-      text: "Total Products",
-      iconBackgroundColor: "#EA580C",
-      path: "/dashboard/product",
-    },
-    {
-      img: <BiCategory className="text-white" size={30}></BiCategory>,
-      count: summary?.totalCategory,
-      text: "Total Category",
-      iconBackgroundColor: "#EFBF02",
-      path: "/dashboard/category",
-    },
-    {
-      img: <SiBrandfolder className="text-white" size={30}></SiBrandfolder>,
-      count: summary?.totalBrand,
-      text: "Total Brand",
-      iconBackgroundColor: "#8B5CF6",
-      path: "/dashboard/brand",
-    },
-    {
-      img: <FaStore className="text-white" size={30}></FaStore>,
-      count: summary?.totalStore,
-      text: "Total Store",
-      iconBackgroundColor: "#16A34A",
-      path: "/dashboard/store",
-    },
-  ];
+  
+
+    const items = [
+      {
+        img: (
+          <AiOutlineDollarCircle
+            className="text-white"
+            size={30}
+          ></AiOutlineDollarCircle>
+        ),
+        count: `${defaultSettings?.settings?.currency || "$"}${
+          summary?.totalRevenue
+        }`,
+        text: "Total Revenue",
+        backgroundColor: "#EA580C",
+      },
+      {
+        img: <VscGraph className="text-white" size={30}></VscGraph>,
+        count: summary?.totalSales,
+        text: "Total Sales",
+        backgroundColor: "#EFBF02",
+        path: "/dashboard/invoice",
+      },
+      {
+        img: <BiUserCircle className="text-white" size={30}></BiUserCircle>,
+        count: summary?.totalUsers,
+        text: "Total Users",
+        backgroundColor: "#8B5CF6",
+        path: "/dashboard/user",
+      },
+      {
+        img: <FiUsers className="text-white" size={30}></FiUsers>,
+        count: summary?.totalCustomers,
+        text: "Total Customers",
+        backgroundColor: "#16A34A",
+        path: "/dashboard/customer",
+      },
+      {
+        img: <BsFillCartFill className="text-white" size={30}></BsFillCartFill>,
+        count: summary?.totalProducts,
+        text: "Total Products",
+        backgroundColor: "#EA580C",
+        path: "/dashboard/product",
+      },
+      {
+        img: <BiCategory className="text-white" size={30}></BiCategory>,
+        count: summary?.totalCategory,
+        text: "Total Category",
+        backgroundColor: "#EFBF02",
+        path: "/dashboard/category",
+      },
+      {
+        img: <SiBrandfolder className="text-white" size={30}></SiBrandfolder>,
+        count: summary?.totalBrand,
+        text: "Total Brand",
+        backgroundColor: "#8B5CF6",
+        path: "/dashboard/brand",
+      },
+      {
+        img: <FaStore className="text-white" size={30}></FaStore>,
+        count: summary?.totalStore,
+        text: "Total Store",
+        backgroundColor: "#16A34A",
+        path: "/dashboard/store",
+      },
+    ];
+
+  
+
+// for (let index = 0; index < items.length; index++) {
+//   const element = items[index];
+//   console.log(element?.backgroundColor)
+  
+// }
 
   return (
-    <div className="grid grid-cols-2 lg:grid-cols-4 gap-3 md:gap-5 my-2 p-2">
+    <>
+    {
+      items?.length > 0 && (<div className="grid grid-cols-2 lg:grid-cols-4 gap-3 md:gap-5 my-2 p-2">
       {items &&
         items?.map((item, i) => (
           <Link key={i} to={item?.path}>
@@ -105,7 +118,7 @@ const TotalCalculations = () => {
               className="flex flex-col md:flex-row gap-y-5 justify-between items-center py-5 md:py-7 px-3 md:px-5 rounded-xl bg-gray-100 border border-gray-300"
             >
               <div
-                className={`h-[60px] p-5 z-10 rounded-lg flex justify-center items-center w-[60px] bg-[${item?.iconBackgroundColor}]`}
+                className={`h-[60px] p-5 z-10 rounded-lg flex justify-center items-center w-[60px]`} style={{backgroundColor: item?.backgroundColor}}
               >
                 {item?.img}
               </div>
@@ -116,7 +129,9 @@ const TotalCalculations = () => {
             </div>
           </Link>
         ))}
-    </div>
+    </div>)
+    }
+    </>
   );
 };
 
