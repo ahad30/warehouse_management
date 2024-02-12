65dd5d3bccae993dc24199f065f975ba9aa0438e
Akramul Hoque <19862764-akramulhoque@users.noreply.gitlab.com>
2024-02-12 13:08:46 +0600

    elouquent relationship changes

--- Backend/app/Models/Warehouse.php
+++ Backend/app/Models/Warehouse.php
@@ -20,24 +20,14 @@ class Warehouse extends Model
         'email',
         'site_link',
     ];
-    // protected $visible = [
-    //     'name',
-    //     'slug',
-    //     'country',
-    //     'city',
-    //     'address',
-    //     'phone',
-    //     'email',
-    //     'site_link',
-    // ];
-    public static $rules =  [
-        'name' => 'required|string|max:255',
-        'slug' => 'required|string|max:255',
-        'country' => 'required|string',
-        'city' => 'required|string',
-        'address' => 'nullable',
-        'phone' => 'required|regex:/^([0-9\s\-\+\(\)]*)$/|min:10',
-        'email' => 'required|email|string|lowercase',
-        'site_link' => 'nullable|active_url',
-    ];
+
+    public function categories()
+    {
+        return $this->hasMany(Category::class);
+    }
+
+    public function brands()
+    {
+        return $this->hasMany(Brand::class);
+    }
 }
