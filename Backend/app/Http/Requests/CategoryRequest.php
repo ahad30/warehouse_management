4aec47f7caebf83e9ca5875e6cb0b6262854b4d9
Akramul Hoque <19862764-akramulhoque@users.noreply.gitlab.com>
2024-02-17 17:45:58 +0600

    code modified

--- Backend/app/Http/Requests/CategoryRequest.php
+++ Backend/app/Http/Requests/CategoryRequest.php
@@ -5,7 +5,6 @@ namespace App\Http\Requests;
 use Illuminate\Contracts\Validation\Validator as Validation;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
-use Illuminate\Validation\Rule;
 
 class CategoryRequest extends FormRequest
 {
