8032bdbce476473194bcc3ea834f9b1754e73eaa
Mohiminul Islam Ahad <122167069+ahad30@users.noreply.github.com>
2024-02-22 18:26:02 +0600

    ahad

--- Frontend/src/Dashboard/History/AddHistory.jsx
+++ Frontend/src/Dashboard/History/AddHistory.jsx
@@ -4,7 +4,7 @@ import SubmitButton from "../../components/Reusable/Buttons/SubmitButton";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-hot-toast";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import UseTitle from "../../components/Reusable/UseTitle/UseTitle";
 import { useGetStoresQuery } from "../../features/Store/storeApi";
@@ -13,20 +13,34 @@ import { useGetProductsQuery } from "../../features/Product/productApi";
 
 const AddHistory = () => {
   UseTitle("Add History");
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit} = useForm();
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { data: storesData } = useGetStoresQuery();
   const {data : productsData } = useGetProductsQuery();
   const [addHistory, { isLoading, isError, error, isSuccess, data }] =
   useAddHistoryMutation();
+  const [selectedFromWarehouse, setSelectedFromWarehouse] = useState("");
+  const [selectedToWarehouse, setSelectedToWarehouse] = useState("");
 
+  const handleFromWarehouseChange = (event) => {
+    const selectedValue = event.target.value;
+    setSelectedFromWarehouse(selectedValue);
+    if (selectedToWarehouse === selectedValue) {
+      setSelectedToWarehouse("");
+    }
+  };
+
+  const handleToWarehouseChange = (event) => {
+    setSelectedToWarehouse(event.target.value);
+  };
+  
   const onSubmit = async (data) => {
     ('from_warehouse_id' , data?.from_warehouse_id);
     ('to_warehouse_id' , data?.to_warehouse_id);
     ('product_id' , data?.product_id);
     addHistory(data);
-
+    console.log(data);
   };
 
   useEffect(() => {
@@ -56,41 +70,61 @@ const AddHistory = () => {
     <DashboardBackground>
       <h2 className="text-xl my-5 font-semibold">Add History</h2>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="grid lg:grid-cols-2 gap-5">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+        <label className="input-group">
+        <span className="font-semibold">
+          Warehouse<span className="text-red-500 p-0">*</span>
+        </span>
+        <select
+          className="select select-bordered w-full"
+          required
+          value={selectedFromWarehouse}
+          onChange={handleFromWarehouseChange}
+        >
+          <option value={""}>From Warehouse</option>
+          {storesData?.data?.map((data) => (
+            <option key={data?.id} value={data?.id}>
+              {data?.name}
+            </option>
+          ))}
+        </select>
+      </label>
+      <label className="input-group">
+        <span className="font-semibold">
+          Warehouse<span className="text-red-500 p-0">*</span>
+        </span>
+        <select className="select select-bordered w-full"
+        value={selectedToWarehouse}
+        onChange={handleToWarehouseChange}
+        required>
+          <option value={""}>To Warehouse</option>
+          {storesData?.data?.filter((data) => data.id !== selectedFromWarehouse).map((data) => (
+              <option key={data?.id} value={data?.id}>
+                {data?.name}
+              </option>
+            ))}
+        </select>
+      </label>
+
         <label className="input-group">
             <span className="font-semibold">
-              Warehouse<span className="text-red-500 p-0">*</span>
+              Product<span className="text-red-500 p-0">*</span>
             </span>
             <select
               className="select select-bordered w-full"
-              required
-              {...register("from_warehouse_id")}
+              
+              {...register("product_id")}
             >
-              <option value={""}>Select Warehouse Info</option>
-              {storesData?.data?.map((data) => (
+              <option value={""}>Select Product</option>
+              {productsData?.data?.map((data) => (
                 <option key={data?.id} value={data?.id}>
                   {data?.name}
                 </option>
               ))}
             </select>
           </label>
-        <label className="input-group">
-            <span className="font-semibold">
-              Warehouse<span className="text-red-500 p-0">*</span>
-            </span>
-            <select
-              className="select select-bordered w-full"
-              required
-              {...register("to_warehouse_id")}
-            >
-              <option value={""}>Select Warehouse Info</option>
-              {storesData?.data?.map((data) => (
-                <option key={data?.id} value={data?.id}>
-                  {data?.name}
-                </option>
-              ))}
-            </select>
-          </label>
+
+
 
           {/* <label className="input-group">
             <span className="font-semibold min-w-[110px]">
@@ -127,7 +161,7 @@ const AddHistory = () => {
           </label> */}
         </div>
         <SubmitButton
-          title={isLoading ? "Adding Category..." : "Add History"}
+          title={isLoading ? "Adding History..." : "Add History"}
           icon={<BiSolidDuplicate size={20} />}
           isLoading={isLoading}
         />
