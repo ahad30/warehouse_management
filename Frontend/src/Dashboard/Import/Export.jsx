4c4eb59f23e5014d8e719edd590b1c89b5eb5f0e
Ummepinki <salmaumme888@gmail.com>
2024-02-29 17:37:07 +0600

    import button fixed

--- Frontend/src/Dashboard/Import/Export.jsx
+++ Frontend/src/Dashboard/Import/Export.jsx
@@ -25,28 +25,48 @@ const Export = () => {
     useGetExportMutation();
   const [getallWarehouseCsv, { data: allWareHouseData }] =
     useGetAllExportsMutation();
-
-  const onSubmit = (formData) => {
-    const warehouseId = formData.warehouse_id;
-    console.log(warehouseId);
-    getSpecificWarehouseCsv(warehouseId);
-  };
+  const [track, setTrack] = useState("");
+  // const onSubmit = (formData) => {
+  //   const warehouseId = formData.warehouse_id;
+  //   console.log(warehouseId);
+  //   getSpecificWarehouseCsv(warehouseId);
+  // };
   const { data: storesData } = useGetStoresQuery();
 
-  useEffect(() => {
-    if (WareHouseData?.url) {
-      setUrl(WareHouseData?.url);
-    }
-    if (allWareHouseData?.url) {
-      setUrl(allWareHouseData?.url);
-    }
-  }, [WareHouseData, allWareHouseData]);
+  // useEffect(() => {
+  //   if (WareHouseData?.url) {
+  //     setUrl(WareHouseData?.url);
+  //   }
+  //   if (allWareHouseData?.url) {
+  //     setUrl(allWareHouseData?.url);
+  //   }
+  // }, [WareHouseData, allWareHouseData]);
 
   const handleAllCsvData = () => {
     getallWarehouseCsv();
   };
 
-  console.log(allWareHouseData);
+  // console.log(allWareHouseData);
+  // console.log(url);
+
+  const generateCsvData = async (from) => {
+    if (!track) {
+      alert("plz slelect first");
+    } else if (from === "All") {
+      // console.log("hefdfsllo");
+
+      const res = await getallWarehouseCsv().unwrap();
+      setUrl(res?.url);
+      setShowButton(true);
+    } else if (from === "warehouse") {
+      // console.log("hello");
+      const res = await getSpecificWarehouseCsv(track?.id).unwrap();
+      setUrl(res?.url);
+      setShowButton(true);
+    }
+  };
+
+  console.log(track);
   console.log(url);
   return (
     <DashboardBackground>
@@ -74,13 +94,22 @@ const Export = () => {
             setModalIsOpen={setModalIsOpen}
           />
         </div>
-        <form onSubmit={handleSubmit(onSubmit)}>
+
+        {/* <button
+          onClick={() => handleAllCsvData()}
+          type="button"
+          className="bg-[#e74c3c]  px-3 py-2 rounded-md text-white"
+        >
+          All
+        </button> */}
+
+        <>
           <label className="font-bold text-lg mt-5 ">Export :</label>
           <div className="mt-2 border bg-[#F3F4F6] rounded">
             <div className="flex justify-between">
               <div className="flex space-x-2 items-center m-1">
                 <button
-                  onClick={() => handleAllCsvData}
+                  onClick={() => setTrack("All")}
                   type="button"
                   className="bg-[#e74c3c]  px-3 py-2 rounded-md text-white"
                 >
@@ -92,7 +121,10 @@ const Export = () => {
                       // onChange={()=>}
                       className="select select-bordered w-full max-w-xs"
                       required
-                      {...register("warehouse_id")}
+                      // {...register("warehouse_id")}
+                      onChange={(e) =>
+                        setTrack({ name: "warehouse", id: e.target.value })
+                      }
                     >
                       <option value={""}>Select Warehouse Info</option>
                       {storesData?.data?.map((data) => (
@@ -108,7 +140,7 @@ const Export = () => {
                 <div className="m-3 flex">
                   {showButton && (
                     <a
-                      href={WareHouseData?.url}
+                      href={url}
                       className="cursor-pointer m-auto border px-2 py-1 text-[#e74c3c]  rounded text-sm shadow transition duration-300"
                       rel="noopener noreferrer"
                       download
@@ -116,7 +148,13 @@ const Export = () => {
                       Download
                     </a>
                   )}
-                  <div onClick={() => setShowButton(!showButton)}>
+                  <div
+                    onClick={() =>
+                      generateCsvData(
+                        track?.name === "warehouse" ? track?.name : track
+                      )
+                    }
+                  >
                     <button
                       type="submit"
                       className="inline-flex items-center py-2.5 px-3 ms-2 text-sm font-medium text-white bg-[#e74c3c] rounded"
@@ -132,7 +170,7 @@ const Export = () => {
             {/* Import download as CSV file */}
             <ImportAsCSV data={filterData} />
           </div>
-        </form>
+        </>
       </>
     </DashboardBackground>
   );
