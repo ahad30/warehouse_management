8c4ccce741d0b7d045a4c423ad6bcbc0ffa181cb
Ummepinki <salmaumme888@gmail.com>
2024-02-29 15:09:35 +0600

    csv modal added

--- Frontend/src/Dashboard/Products/Imageview.jsx
+++ Frontend/src/Dashboard/Products/Imageview.jsx
@@ -10,8 +10,8 @@ const Imageview = ({ modalIsOpen, setModalIsOpen, product }) => {
         onClick={() => setModalIsOpen(false)}
       ></div>
       <div className="flex items-center min-h-screen  px-4 py-8">
-        <div className="relative w-full  p-4 mx-auto bg-white rounded-md shadow-lg">
-          <div>
+        <div className="relative w-full p-4 mx-auto bg-white rounded-md shadow-lg">
+          <div className="grid grid-cols-3 gap-2">
             {imgesArray?.map((item, index) => (
               <img
                 key={index}
@@ -19,8 +19,17 @@ const Imageview = ({ modalIsOpen, setModalIsOpen, product }) => {
                   import.meta.env.VITE_REACT_APP_PUBLIC_IMAGE_PORT
                 }${item}`}
                 alt=""
+                className="w-full h-[250px] object-cover rounded-lg"
               />
             ))}
+          </div>
+          <div className="flex justify-end">
+            <button
+              className="bg-red-600  mt-2 p-3.5 flex text-white rounded-md outline-none border ring-offset-2 ring-red-500 focus:ring-2"
+              onClick={() => setModalIsOpen(false)}
+            >
+              Cancel
+            </button>
           </div>
         </div>
       </div>
