0cfc7d814d803b6c0f06044926f08bea30b80ebc
unknown <naimurk781@gmail.com>
2024-03-07 11:53:00 +0600

    s

--- Frontend/src/features/History/historyApi.jsx
+++ Frontend/src/features/History/historyApi.jsx
@@ -14,8 +14,19 @@ const historyApi = apiSlice.injectEndpoints({
       invalidatesTags: ["getHistory"],
     }),
     getHistory: build.query({
-      query: ({ pageNumber, query }) => ({
-        url: `/histories?page=${pageNumber ? pageNumber : 1}&query=${query}`,
+      query: ({
+        pageNumber,
+        query,
+        brand_id,
+        category_id,
+        to_warehouse,
+        starting_date,
+        ending_date,
+        from_warehouse,
+      }) => ({
+        url: `/histories?page=${
+          pageNumber ? pageNumber : 1
+        }&&query=${query}&brand_id=${brand_id}&category_id=${category_id}&&from_warehouse=${from_warehouse}&&to_warehouse=${to_warehouse}&starting_date=${starting_date}&&ending_date=${ending_date}`,
       }),
       providesTags: ["Histories"],
     }),
