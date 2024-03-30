4625a93b8e1af5b74bed4e6c693e84a2d343b877
Akramul Hoque <19862764-akramulhoque@users.noreply.gitlab.com>
2024-03-30 13:16:18 +0600

    product image updated:

--- Backend/app/Http/Controllers/SearchProductController.php
+++ Backend/app/Http/Controllers/SearchProductController.php
@@ -11,14 +11,15 @@ class SearchProductController extends Controller
 
     public function index(Request $request)
     {
-        $product = Product::where('scan_code', $request->scan_code)->with('histories.fromWarehouseId','histories.toWarehouseId','histories.user','getCategory','getBrand','warehouse','productImages')->latest()->first();
-        
+        $product = Product::where('scan_code', $request->scan_code)->with('histories.fromWarehouseId', 'histories.toWarehouseId', 'histories.user', 'getCategory', 'getBrand', 'warehouse', 'productImages')->latest()->first();
+
         if (!$product) {
             return response()->json([
-                'status' => true,
+                'status' => false,
                 'message' => 'Product not found',
-            ], 200);
+            ], 404);
         }
+
         return response()->json([
             'status' => true,
             'data' =>  $product,
